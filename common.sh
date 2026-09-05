f99edb57956de2363076dde8ce39d9c606ed6474
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-05 12:04:43 +0530

    actions

--- common.sh
+++ common.sh
@@ -26,9 +26,9 @@ check_root(){
     fi
 }   
 
-END_TIME=$(date +%S)
 
 print_total_time(){
+END_TIME=$(date +%S)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
-echo "$(date "+%y-%m-%d %H:%M:%S") | excuting time is : $TOTAL_TIME in seconds | tee -a $LOGS_FILE
+echo "$(date "+%y-%m-%d %H:%M:%S") | excuting time is : $TOTAL_TIME in seconds " | tee -a $LOGS_FILE
 }
