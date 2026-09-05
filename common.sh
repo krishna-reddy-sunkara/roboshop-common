9951b5f5ccdc3a07b85967b03e5377860c3e9197
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-05 14:22:48 +0530

    actions

--- common.sh
+++ common.sh
@@ -71,8 +71,15 @@ java_setup(){
 
 python_setup(){
 
+    #dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    #VALIDATE $? "Installing Python"
+    #cd /app 
+    #pip3 install -r requirements.txt &>>$LOGS_FILE
+    #VALIDATE $? "Installing dependencies"
+    
     dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
     VALIDATE $? "Installing Python"
+
     cd /app 
     pip3 install -r requirements.txt &>>$LOGS_FILE
     VALIDATE $? "Installing dependencies"
