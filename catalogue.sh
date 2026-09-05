e114a37143af4bb20e51a0828e931c3d768a016a
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-05 12:50:22 +0530

    actions

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+nodejs_setup
+
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
--- common.sh
+++ common.sh
@@ -6,6 +6,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+MONGODB_HOST=mongodb.daws-92s.store
+$SCRIPT_DIR=$PWD
 
 START_TIME=$(date +%S)
 mkdir -p $LOGS_FOLDER
@@ -27,7 +29,60 @@ VALIDATE(){
     fi
 }   
 
+app_set(){
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
 
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading $app_name code"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/$app_name.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip $app_name code"
+}
+
+nodejs_setup(){
+
+    dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS Default version"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
+
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
+}
+
+systemd_setup(){
+
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $app_name  &>>$LOGS_FILE
+    systemctl start $app_name
+    VALIDATE $? "Starting and enabling $app_name"
+}
+app_restart(){
+    systemctl restart $app_name
+VALIDATE $? "Restarting $app_name"
+}
 print_total_time(){
 END_TIME=$(date +%S)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
