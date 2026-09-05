dd617343c96f11e3ea1c20b3a8ed9dd9df17b0cc
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-05 11:42:32 +0530

    actions

---
+++ common.sh
@@ -0,0 +1,33 @@
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%S)
+
+echo "$(date "+%y-%m-%d %H:%M:%S")|script started excuting at: $(date) | tee -a $LOGS_FILE
+check_root(){
+  if [ $USERID -ne 0 ]; then
+      echo " please run this script with sudo user " | tee -a $LOGS_FILE
+      exit 1
+  fi
+}
+   
+    mkdir -p $LOGS_FOLDER
+
+ VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "$(date "+%y-%m-%d %H:%M:%S") |$2 .... failure " | tee -a $LOGS_FILE
+    exit 1
+    else 
+    echo "$( date "+%y-%m-%d %H:%M:%S") |$2 .... success " | tee -a $LOGS_FILE
+    fi
+}   
+
+END_TIME=$(date +%S)
+
+print_total_time(){
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "$( date "+%y-%m-%d %H:%M:%S") |excuting time is : $TOTAL_TIME in seconds | tee -a $LOGS_FILE
+}
---
+++ mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "coping mongo.repo "
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "installing mongodb "
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "enabling mongodb "
+
+systemctl start mongod &>>$LOGS_FILE
+VALIDATE $? "starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "allowing remote connections"
+ 
+systemctl restart mongod &>>$LOGS_FILE
+VALIDATE $? "restarting mongodb"
+ 
+print_total_time
