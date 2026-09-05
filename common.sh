9e0997eb5dedfe9bb0c61db3b6e8c05f40af351a
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-05 12:18:25 +0530

    actions

--- common.sh
+++ common.sh
@@ -2,27 +2,28 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 START_TIME=$(date +%S)
+mkdir -p $LOGS_FOLDER
 
- mkdir -p $LOGS_FOLDER
+echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
 
-echo "$(date "+%y-%m-%d %H:%M:%S")|script started excuting at: $(date) | tee -a $LOGS_FILE
-
-check_root(){ 
-
-  if [ $USERID -ne 0 ]; then
-      echo " please run this script with sudo user " | tee -a $LOGS_FILE
-      exit 1
-  fi
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+        exit 1
+    fi
 }
-   
- VALIDATE(){
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo "$(date "+%y-%m-%d %H:%M:%S") | $2 .... failure " | tee -a $LOGS_FILE
-    exit 1
-    else 
-    echo "$(date "+%y-%m-%d %H:%M:%S") | $2 .... success " | tee -a $LOGS_FILE
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }   
 
@@ -32,3 +33,5 @@ END_TIME=$(date +%S)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "$(date "+%y-%m-%d %H:%M:%S") |excuting time is : $TOTAL_TIME in seconds " | tee -a $LOGS_FILE
  }
+
+ 
